87df4c8e414e2519a51847a510af86dab04ad750
Dima <dimaabramov555@gmail.com>
2019-04-09 14:34:34 +0300

    Updates

---
+++ precache-manifest.059f62b24bc70d480ac4109c9cdd11a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f3e5466f52a2d78ca1ed0a0d4e3ff8",
+    "url": "/Akti/static/media/start.45f3e546.png"
+  },
+  {
+    "revision": "98fddcff14645dbd44c9",
+    "url": "/Akti/static/js/runtime~main.30d4ad00.js"
+  },
+  {
+    "revision": "00e2c8953ed8e6bb7733",
+    "url": "/Akti/static/js/main.2c5350f6.chunk.js"
+  },
+  {
+    "revision": "beaf57dcd22a605d18d2",
+    "url": "/Akti/static/js/2.6083f54e.chunk.js"
+  },
+  {
+    "revision": "00e2c8953ed8e6bb7733",
+    "url": "/Akti/static/css/main.95addb2f.chunk.css"
+  },
+  {
+    "revision": "d58f1554c773dfee656a2ae77144e390",
+    "url": "/Akti/index.html"
+  }
+];
